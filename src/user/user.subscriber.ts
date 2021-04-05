b9260c696733791e37169d24a12d210cfb0d1f0b
june <lzao@naver.com>
2021-04-05 23:25:56 +0900

    UserSubscriber의 추석을 추가합니다

--- src/user/user.subscriber.ts
+++ src/user/user.subscriber.ts
@@ -12,14 +12,27 @@ export class UserSubscriber implements EntitySubscriberInterface<User> {
         return User;
     }
 
+    /**
+     * 비밀번호를 암호화 합니다.
+     * @param entity 
+     */
     async hashPassword(entity: User): Promise<void> {
         entity.user_pw = await bcrypt.hash(entity.user_pw, bcrypt.genSaltSync(10));
     }
 
+    /**
+     * 회원 정보 추가 시 비밀번호를 암호화하여 저장합니다.
+     * @param event 
+     * @returns 
+     */
     beforeInsert(event: InsertEvent<User>): Promise<void> {
         return this.hashPassword(event.entity);
     }
 
+    /**
+     * 회원 비밀번호 수정 시 기존 비밀번호와 같지 않다면 암호화하여 저장합니다.
+     * @param param0 
+     */
     async beforeUpdate({entity, databaseEntity}: UpdateEvent<User>): Promise<void> {
         if (entity.user_pw !== databaseEntity?.user_pw) {
             await this.hashPassword(entity);
